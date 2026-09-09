2958f46171ef4bd3e5f5df16c4dac8b09f8796c4
MossimoB <2585451@edu.vaniercollege.qc.ca>
2026-09-08 21:26:49 -0400

    added instructions so that I don't have to go back and forth between tabs/pdf

--- Assignment01/src/main/java/mossimo/bianco/assignment01/App.java
+++ Assignment01/src/main/java/mossimo/bianco/assignment01/App.java
@@ -6,6 +6,47 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
+/**
+ * INSTRUCTIONS
+ * 
+ * basically recreate monkeytype (kind of) in javafx
+ * 
+ * virtual keyboard using buttons (don't need all, but i will do all)
+ * 
+ * textfield to display text to type
+ * textfield to display typed response (matched to physical keyboard)
+ * 
+ * need key_pressed/released (key inputs)
+ * must show key being pressed/released when it is pressed/released (change opacity or background)
+ * if user presses external key (not shown in window) then error message
+ * 
+ * next button to move to next displayed text
+ * when pressed, show new text, clear user text field, show 2 of counter
+ * 
+ * reset button to return to input 1 of 6
+ * method to track correct/incorrect keystrokes
+ * 
+ * allow backspace
+ * 
+ */
+
+/**
+ * SAMPLE TEXT FOR TESTING
+ * 
+ * Try typing this text. Do it as quickly and as accurately as you can.
+ * 
+ * Next type another line of input data.
+ * 
+ * The quick brown fox jumps over the lazy dog.
+ * 
+ * FIve big quacking zephyrs jolt my wax bed.
+ * 
+ * Sympathizing would fix Quaker objectives.
+ * 
+ * A large fawn jumped quickly over the white zinc boxes.
+ * 
+ */
+
 
 /**
  * JavaFX App
